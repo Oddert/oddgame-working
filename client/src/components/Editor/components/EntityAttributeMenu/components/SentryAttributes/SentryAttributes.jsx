578092225e37467298aa8d5c48480f8d73624deb
Robyn F H Veitch <oddert@gmail.com>
2023-04-30 13:01:26 +0100

    added speed control  for sentry

--- client/src/components/Editor/components/EntityAttributeMenu/components/SentryAttributes/SentryAttributes.jsx
+++ client/src/components/Editor/components/EntityAttributeMenu/components/SentryAttributes/SentryAttributes.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { Fragment, useCallback } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { editChangeCell } from '../../../../../../actions'
 
 import DirectionSelector from '../DirectionSelector/DirectionSelector'
 
 const SentryAttributes = () => {
+    const dispatch = useDispatch()
+    const { entity, y, x } = useSelector(state => state.edit.painter.focus)
+
+    const speed = entity?.speed
+ 
+    const handleChangeTime = useCallback((e) => {
+        const cell = {
+            ...entity,
+            speed: e.currentTarget.value,
+        }
+        dispatch(editChangeCell(y, x, cell))
+    }, [
+        dispatch, entity, y, x,
+    ])
+    
     return (
-        <DirectionSelector
-            entityName='Sentry'
-            entityType='sentry'
-        />
+        <Fragment>
+            <DirectionSelector
+                entityName='Sentry'
+                entityType='sentry'
+            />
+            <div className='Selector'>
+                <h5>Shooter Direction</h5>
+                <div className='Selector__category centred'>
+                    <div className='Selector__option'>
+                        <label htmlFor='SentryAttributes-speed'>
+                            Speed: {speed}
+                        </label>
+                        <input
+                            id='SentryAttributes-speed'
+                            onChange={handleChangeTime}
+                            max='9'
+                            min='3'
+                            type='range'
+                            value={speed}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Fragment>
     )
 }
 
